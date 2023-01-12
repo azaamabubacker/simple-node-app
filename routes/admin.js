65c5183e48744e64830f0d51618b3e44acc030c6
azaamabubacker <cleancodelk@gmail.com>
2023-01-12 23:40:23 +0530

    added variable to save the data

--- routes/admin.js
+++ routes/admin.js
@@ -4,13 +4,16 @@ const router = express.Router();
 
 const rootDir = require("../utill/path");
 
-router.get("/", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+const products = [];
+
+router.get("/add-product", (req, res, next) => {
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
-router.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/admin");
+router.post("/add-product", (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect("/");
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
